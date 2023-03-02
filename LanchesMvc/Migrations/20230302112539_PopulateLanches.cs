ad4ec05570b30470189c1286eb83efb5c4de99b3
Pedro Henrique Costa Miguez <pedrohcmiguez@gmail.com>
2023-03-02 10:20:12 -0300

    Edited List View

--- LanchesMvc/Migrations/20230302112539_PopulateLanches.cs
+++ LanchesMvc/Migrations/20230302112539_PopulateLanches.cs
@@ -12,19 +12,19 @@ namespace LanchesMvc.Migrations
         {
             migrationBuilder.Sql("INSERT INTO Lanches" +
             "(CategoriaId, Nome, DescricaoCurta, DescricaoDetalhada, Preco, ImagemUrl, ImagemThumbnailUrl, IsLanchePreferido, EmEstoque)" +
-            "VALUES(1,'Big Burger', 'Hamburguer estilo McDonalds','Um Hambúrger Americano bem elaborado e saboroso',39.90,'hamb01.jpg','hamb01thumb.jpg',0,1)");
+            "VALUES(1,'Big Burger', 'Hamburguer estilo McDonalds','Um Hambúrger Americano bem elaborado e saboroso',39.90,'lanche01.png','lanche01.png',0,1)");
 
             migrationBuilder.Sql("INSERT INTO Lanches" +
             "(CategoriaId, Nome, DescricaoCurta, DescricaoDetalhada, Preco, ImagemUrl, ImagemThumbnailUrl, IsLanchePreferido, EmEstoque)" +
-            "VALUES(1,'Hot Dog', 'Melhor Cachorro quente da cidade','Acompanha batata palha e milho verde',9.90,'hot01.jpg','hot01thumb.jpg',1,1)");
+            "VALUES(1,'Hot Dog', 'Melhor Cachorro quente da cidade','Acompanha batata palha e milho verde',9.90,'lanche02.png','lanche02.png',1,1)");
 
             migrationBuilder.Sql("INSERT INTO Lanches" +
             "(CategoriaId, Nome, DescricaoCurta, DescricaoDetalhada, Preco, ImagemUrl, ImagemThumbnailUrl, IsLanchePreferido, EmEstoque)" +
-            "VALUES(1,'Pitbull','X-Tudo', '2 Carnes de Hamburger e muito mais', 59.90,'xtudo01.jpg','xtudothumb.jpg',1,1)");
+            "VALUES(1,'Pitbull','X-Tudo', '2 Carnes de Hamburger e muito mais', 59.90,'lanche03.png','lanche03.png',1,1)");
 
             migrationBuilder.Sql("INSERT INTO Lanches" +
             "(CategoriaId, Nome, DescricaoCurta, DescricaoDetalhada, Preco, ImagemUrl, ImagemThumbnailUrl, IsLanchePreferido, EmEstoque)" +
-            "VALUES(2,'Frango da Malásia', 'Hamburguer de frango','Um Hambúrger Feito com Frango da Malásia',49.90,'hamb04.jpg','hamb04thumb.jpg',0,0)");
+            "VALUES(2,'Frango da Malásia', 'Hamburguer de frango','Um Hambúrger Feito com Frango da Malásia',49.90,'lanche04.png','lanche04.png',0,0)");
         }
 
         /// <inheritdoc />
